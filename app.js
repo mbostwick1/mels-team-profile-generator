02fc0f167184ec9fecadd762cc7e537a9d10c905
Melanie Bostwick <mbostwick1@gmail.com>
2020-11-14 00:27:12 -0500

    add class to eemployee.js and working question to app.js

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+    }
+];
+
+inquirer.prompt(questions).then(function(data){
+    console.log(data.name);
+})
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
